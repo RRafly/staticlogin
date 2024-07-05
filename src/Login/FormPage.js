df08831a2a79395719c642befb3361d28d5846c1
RRafly <raflyibnu532@gmail.com>
2024-07-05 08:48:49 +0700

    fix center

--- src/Login/FormPage.js
+++ src/Login/FormPage.js
@@ -1,32 +1,37 @@
 import React from 'react'
-import { Form, Button, Card } from 'react-bootstrap'
+import { Form, Button, Card, Col } from 'react-bootstrap'
 
 export default function FormPage() {
   const submit = (e) => {
     e.preventDefault()
-    console.log("submit")
-    // tampilan masih belum sempurna
+    console.log("tombol submit di tekan")
+    // submitt
   }
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Form onSubmit={submit}>
-          <Form.Group className="mb-3">
-            <Form.Label>User</Form.Label>
-            <Form.Control type="name" placeholder="Masukan Nama User" />
-          </Form.Group>
+    <Col style={{
+      justifyContent: 'center',
+      height: '100vh',
+      display: 'flex'
+    }}>
+      <Card style={{ width: '18rem', height: '20rem'}} className='mt-5'>
+        <Card.Body>
+          <Card.Title className='co'>Login simpel</Card.Title>
+          <Form onSubmit={submit}>
+            <Form.Group className="mb-3">
+              <Form.Label>User</Form.Label>
+              <Form.Control type="name" placeholder="Masukan Nama User" />
+            </Form.Group>
 
-          <Form.Group className="mb-3">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Masukan Password" />
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
-      </Card.Body>
-    </Card>
+            <Form.Group className="mb-3">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Masukan Password" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+    </Col>
   )
 }
